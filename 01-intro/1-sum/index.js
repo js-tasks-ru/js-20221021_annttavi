ff711dd5f99c64402422038a51c14101805e5a9b
ann ttavi <annttavi@yandex.ru>
2022-10-23 23:05:01 +0300

    firsttask

--- 01-intro/1-sum/index.js
+++ 01-intro/1-sum/index.js
@@ -1,10 +1,15 @@
 /**
- * Sum of two numbers
+ * mysum: Sum of two numbers
  *
  * @param {number} m first number
  * @param {number} n second number
  * @returns {number}
  */
-export const sum = (m, n) => {
-
-};
+export default function mysum (a, b) { 
+    if (Number(a)===a && Number(b)===b) {
+        return a+b;
+    }
+    else {
+        return NaN;//"Ошибка. Проверьте параметры";
+    }
+}
