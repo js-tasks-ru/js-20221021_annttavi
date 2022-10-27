c2ed67f17feb345e3335a5193b227b9f35ae4b2c
ann ttavi <annttavi@yandex.ru>
2022-10-27 23:37:47 +0300

    homework2_1

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -4,6 +4,17 @@
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
-export function sortStrings(arr, param = 'asc') {
+ function doSort_asc (a, b) {
+    return Intl.Collator(['en','ru'],{ caseFirst: 'upper'}).compare(a, b);
+  }
+function doSort_desc (a, b) {
+    return Intl.Collator(['ru','en'],{ caseFirst: 'lower' }).compare(b, a);
+  }
 
+export function sortStrings(arr, param = 'asc') {
+    let arr_out = arr.slice();
+    if (param==="asc")  arr_out.sort(doSort_asc)
+    else  if (param==="desc")  arr_out.sort(doSort_desc);
+    return arr_out;
 }
+
