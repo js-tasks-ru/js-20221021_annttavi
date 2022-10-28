e4cca4174e490e64df706ac5db949219848d6cb4
annttavi <annttavi@yandex.ru>
2022-10-28 19:07:43 +0300

    homework 2.2 ver 2

--- 02-javascript-data-types/2-pick/index.js
+++ 02-javascript-data-types/2-pick/index.js
@@ -4,6 +4,33 @@
  * @param {...string} fields - the properties paths to pick
  * @returns {object} - returns the new object
  */
-export const pick = (obj, ...fields) => {
 
-};
+export const pick = (obj, ...fields) => {
+    let arr_out = {};
+    if (Array.isArray(fields) && fields.length>0) {
+        
+        fields.forEach(element => {
+            if (obj[element]) {
+                arr_out[element] =obj[element];    
+            }
+        });
+    }
+    else { 
+        if (obj[fields]) {
+            arr_out[fields] =obj[fields];
+        }
+    }
+    return arr_out;
+}
+/*const arr1=["apple","orange"];
+const arr2="apple";
+const fruits = {
+    apple: 2,
+    orange: 4,
+    banana: 3
+   };
+
+console.log (pick (fruits, "banana","orange"));*/
+
+
+
